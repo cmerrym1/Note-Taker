3cda6c21b180d3999e759be0909a60a2349991d8
Cheri M <cmerrym1@kent.edu>
2022-03-01 22:35:41 -0500

    new note function added

--- server.js
+++ server.js
@@ -2,11 +2,32 @@ const { notes } = require('./db/db.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
-  app.listen(PORT, () => {
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+      );
+    return body;
+  }
+
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
