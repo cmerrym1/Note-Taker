5279d4c057370b47a30194322a0decf02001fa3b
Cheri M <cmerrym1@kent.edu>
2022-03-02 18:48:39 -0500

    commit me

--- server.js
+++ server.js
@@ -1,32 +1,59 @@
-const { notes } = require('./db/db.json');
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const { notes } = require('./db/db.json');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+
+  app.get('/api/notes', (req, res) => {
+    let results = notes;
+    
+    res.json(results);
   });
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-});
-
-function createNewNote(body, notesArray) {
+  app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
+  function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
     fs.writeFileSync(
     path.join(__dirname, './db/db.json'),
     JSON.stringify({ notes: notesArray }, null, 2)
       );
-    return body;
+    return note;
   }
+  
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
