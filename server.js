31399560586531d6e86ef2c4309fccc9cb17cd82
Cheri M <cmerrym1@kent.edu>
2022-03-02 20:04:14 -0500

    refqactor

--- server.js
+++ server.js
@@ -10,6 +10,27 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+function filterByQuery(query, notesArray) {
+  let titleArray = [];
+  let filteredResults = noteArray;
+  if (query.titleTraits) {
+    if (typeof query.titleTraits === 'string') {
+      titleArray = [query.titleTraits];
+    } else {
+      titleTraitsArray = query.titleTraits;
+    }
+    titleArray.forEach(trait => {
+      filteredResults = filteredResults.filter(
+        note => note.titleTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(note => note.text === query.text);
+  }
+  return filteredResults;
+}
+
 function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
@@ -25,11 +46,21 @@ function createNewNote(body, notesArray) {
   return note;
 }
 
-
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
   
 app.get('/api/notes', (req, res) => {
     let results = notes;
-    
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
     res.json(results);
   });
 
@@ -53,7 +84,10 @@ app.get('/api/notes', (req, res) => {
   
   app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-   {
+  
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
       const note = createNewNote(req.body, notes);
       res.json(note);
     }
@@ -65,6 +99,10 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 app.listen(PORT, () => {
