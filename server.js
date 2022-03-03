5adb86f24730fd72576c0d984bf392b5c9b52fb8
Cheri M <cmerrym1@kent.edu>
2022-03-02 19:12:44 -0500

    post route created

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-  app.get('/api/notes', (req, res) => {
+function findById(id, notesArray) {
+  const result = notesArray.filter(note => note.id === id)[0];
+  return result;
+}
+  
+app.get('/api/notes', (req, res) => {
     let results = notes;
     
     res.json(results);
@@ -26,6 +30,15 @@ app.use(express.json());
     }
   });
   
+  app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
   function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
@@ -38,13 +51,9 @@ app.use(express.json());
   
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
     const note = createNewNote(req.body, notes);
     res.json(note);
-    }
+    
 });
 
 app.get('/', (req, res) => {
