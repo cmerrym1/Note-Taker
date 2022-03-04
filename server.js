db3c80a11b09be9f98cc6155436f7432e5defcef
Cheri M <cmerrym1@kent.edu>
2022-03-03 20:39:33 -0500

    save point

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+const store = require('./db/db.json');
+const {notes} = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notes } = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,15 +17,10 @@ function filterByQuery(query, notesArray) {
     filteredResults = filteredResults.filter(note => note.title === query.title);
   }
   if (query.text) {
-    filteredResults = filteredResults.filter(note => note.species === query.text);
+    filteredResults = filteredResults.filter(note => note.text === query.text);
   return filteredResults;
 }
 };
-
-function findById(id, notesArray) {
-  const result = notesArray.filter(note => note.id === id)[0];
-  return result;
-}
 
 function createNewNote(body, notesArray) {
   const note = body;
@@ -36,41 +32,20 @@ function createNewNote(body, notesArray) {
   return note;
 }
 
-function validateNote(note) {
-  if (!note.title || typeof note.title !== 'string') {
-    return false;
-  }
-  if (!note.text || typeof note.text !== 'string') {
-    return false;
-  }
-  return true;
-}
   
 app.get('/api/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+  res.json(store.notes);
 });
 
   app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
+    res.json(store.notes);
   });
   
   app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-    if (!validateNote(req.body)) {
-      res.status(400).send('The note is not properly formatted.');
-    } else {
+    
       const note = createNewNote(req.body, notes);
       res.json(note);
-    }
   });
 
 app.get('/', (req, res) => {
